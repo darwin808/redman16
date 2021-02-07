84e8a0be84c54441c4eb2d2f6d8de1249c60c140
darwin808 <you@domain.com>
2021-02-07 09:50:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addpeople,
+  decrement,
+  deletepeople,
+  increment,
+  updatepeople,
+} from "./Actions/Actions";
 function App() {
+  const counter = useSelector((e) => e.CounterReducer);
+  const dispatch = useDispatch();
+  const peoples = useSelector((e) => e.PeopleReducer.peoples);
+  const [name, setname] = useState("");
+  const [edit, setedit] = useState("");
+  const [idholder, setidholde] = useState(0);
+  const [modal, setmodal] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(addpeople({ name, id: Math.random() }));
+  };
+  const openmodal = (id) => {
+    setmodal(true);
+    setidholde(id);
+  };
+  const handleedit = (e) => {
+    e.preventDefault();
+    dispatch(updatepeople({ edit, idholder }));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {counter}
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}>
+        add
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}>
+        minus
+      </button>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+        />
+      </form>
+      {modal && (
+        <form action="submit" onSubmit={handleedit}>
+          <input
+            type="text"
+            value={edit}
+            onChange={(e) => {
+              setedit(e.target.value);
+            }}
+          />
+        </form>
+      )}
+      {peoples.map((e) => (
+        <div key={e.id}>
+          {e.name}{" "}
+          <button
+            onClick={() => {
+              dispatch(deletepeople(e.id));
+            }}>
+            delete
+          </button>
+          <button
+            onClick={() => {
+              openmodal(e.id);
+            }}>
+            edit
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
